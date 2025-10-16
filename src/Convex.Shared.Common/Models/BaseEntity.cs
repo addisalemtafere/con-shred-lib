868b2be81684db34039c0aee65478a31c2da9bfd
unknown <nathan@convexit.net>
2025-10-16 16:11:19 +0300

    comment resolution on common

--- src/Convex.Shared.Common/Models/BaseEntity.cs
+++ src/Convex.Shared.Common/Models/BaseEntity.cs
@@ -1,11 +1,13 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace Convex.Shared.Common.Models;
 
 /// <summary>
 /// Base entity class for all domain entities
 /// </summary>
-public abstract class BaseEntity
+public abstract class BaseEntity<TKey> 
 {
-    public Guid Id { get; set; } = Guid.NewGuid();
+    public required TKey Id { get; set; } 
 
     public required string CreatedBy { get; set; }
     public required DateTime CreatedAt { get; set; }
@@ -13,4 +15,9 @@ public abstract class BaseEntity
     public required string LastModifiedBy { get; set; }
     public required DateTime LastModifiedAt { get; set; }
     public required string LastModifiedWs { get; set; }
+    public bool IsActive { get; set; }
+    public bool IsDeleted { get; set; }
+    
+    [Timestamp]
+    public byte[]? RowVersion {  get; set; } // used for optimistic concurrency
 }
---
+++ src/Convex.Shared.Common/Models/BaseEntityWithTenant.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Convex.Shared.Common.Models
+{
+    public abstract class BaseEntityWithTenant : BaseEntity<int>
+    {
+        public required string TenantId { get; set; }
+    }
+}
